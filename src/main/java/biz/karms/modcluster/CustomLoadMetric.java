5d9480acd3b468624e1e4e285fa83c07504a467b
Michal Karm Babacek <karm@redhat.com>
2015-03-09 15:44:40 +0100

    Formatting

--- src/main/java/biz/karms/modcluster/CustomLoadMetric.java
+++ src/main/java/biz/karms/modcluster/CustomLoadMetric.java
@@ -1,5 +1,8 @@
 package biz.karms.modcluster;
 
+import org.jboss.modcluster.container.Engine;
+import org.jboss.modcluster.load.metric.impl.AbstractLoadMetric;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -7,55 +10,52 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
-import org.jboss.modcluster.container.Engine;
-import org.jboss.modcluster.load.metric.impl.AbstractLoadMetric;
-
 /**
  * @author Michal Karm Babacek
  */
 public class CustomLoadMetric extends AbstractLoadMetric {
 
-  private String loadfile;
-  private Pattern pattern;
+    private String loadfile;
+    private Pattern pattern;
 
-  @Override
-  public double getLoad(Engine engine) throws Exception {
-    Scanner scanner = null;
-    String groupOneResult = null;
-    try {
-      scanner = new Scanner(new FileInputStream(loadfile), "UTF-8");
-      while (scanner.hasNextLine()) {
-        Matcher matcher = pattern.matcher(scanner.nextLine());
-        if (matcher.matches() && matcher.group(1) != null) {
-          // We ain't gonna read the rest of the file once the load has been found.
-          groupOneResult = matcher.group(1);
-          double load = Double.parseDouble(groupOneResult);
-          CustomLoadMetricLogger.LOGGER.loadFound(load, loadfile);
-          return load;
+    @Override
+    public double getLoad(Engine engine) throws Exception {
+        Scanner scanner = null;
+        String groupOneResult = null;
+        try {
+            scanner = new Scanner(new FileInputStream(loadfile), "UTF-8");
+            while (scanner.hasNextLine()) {
+                Matcher matcher = pattern.matcher(scanner.nextLine());
+                if (matcher.matches() && matcher.group(1) != null) {
+                    // We ain't gonna read the rest of the file once the load has been found.
+                    groupOneResult = matcher.group(1);
+                    double load = Double.parseDouble(groupOneResult);
+                    CustomLoadMetricLogger.LOGGER.loadFound(load, loadfile);
+                    return load;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            CustomLoadMetricLogger.LOGGER.fileNotFound(e, loadfile);
+        } catch (NumberFormatException e) {
+            CustomLoadMetricLogger.LOGGER.cantParseDobule(e, groupOneResult);
+        } finally {
+            if (scanner != null) {
+                scanner.close();
+            }
         }
-      }
-    } catch (FileNotFoundException e) {
-      CustomLoadMetricLogger.LOGGER.fileNotFound(e, loadfile);
-    } catch (NumberFormatException e) {
-      CustomLoadMetricLogger.LOGGER.cantParseDobule(e, groupOneResult);
-    } finally {
-      if (scanner != null) {
-        scanner.close();
-      }
+        CustomLoadMetricLogger.LOGGER.noLoadFound(loadfile);
+        return 0;
     }
-    CustomLoadMetricLogger.LOGGER.noLoadFound(loadfile);
-    return 0;
-  }
 
-  public void setLoadfile(String loadfile) {
-    this.loadfile = loadfile;
-  }
-
-  public void setParseexpression(String parseexpression) {
-    try {
-      pattern = Pattern.compile(parseexpression);
-    } catch (PatternSyntaxException e) {
-      CustomLoadMetricLogger.LOGGER.patternSyntaxWrong(e, parseexpression);
+    public void setLoadfile(String loadfile) {
+        this.loadfile = loadfile;
     }
-  }
+
+    public void setParseexpression(String parseexpression) {
+        try {
+            pattern = Pattern.compile(parseexpression);
+        } catch (PatternSyntaxException e) {
+            CustomLoadMetricLogger.LOGGER.patternSyntaxWrong(e, parseexpression);
+        }
+    }
 }
